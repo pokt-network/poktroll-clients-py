132cb41228cbf0ff36c9ade57396e42d43eb5b42
Bryan White <bryanchriswhite@gmail.com>
2024-11-28 15:17:00 +0100

    wip: add block query client

--- src/poktroll_clients.py
+++ src/poktroll_clients.py
@@ -2,6 +2,8 @@ from os import path
 
 from cffi import FFI
 
+GoRef = int
+
 # Initialize CFFI
 ffi = FFI()
 
@@ -14,6 +16,8 @@ ffi.cdef("""
     
     GoRef NewEventsQueryClient(const char* comet_websocket_url);
     GoRef EventsQueryClientEventsBytes(GoRef selfRef, const char* query);
+    
+    GoRef NewBlockQueryClient(char *comet_websocket_url, char **err);
 """)
 
 # Load the shared object file
@@ -22,13 +26,26 @@ lib = ffi.dlopen(sharedObjectPath)
 
 
 class EventsQueryClient:
-    selfRef: int  # lib.GoRef
+    self_ref: GoRef
 
     def __init__(self, comet_websocket_url: str):
-        self.selfRef = lib.NewEventsQueryClient(comet_websocket_url.encode('utf-8'))
+        self.self_ref = lib.NewEventsQueryClient(comet_websocket_url.encode('utf-8'))
 
-    def EventsBytes(self, query: str) -> int:  # lib.GoRef:
-        return lib.EventsQueryClientEventsBytes(self.selfRef, query.encode('utf-8'))
+    def EventsBytes(self, query: str) -> GoRef:
+        return lib.EventsQueryClientEventsBytes(self.self_ref, query.encode('utf-8'))
 
     def free(self):
-        lib.FreeClient(self.selfRef)
+        lib.FreeClient(self.self_ref)
+
+class BlockQueryClient:
+    self_ref: GoRef
+    err_ptr: ffi.CData
+
+    def __init__(self, comet_websocket_url: str):
+        self.err_ptr = ffi.new("char **")
+        self.self_ref = lib.NewBlockQueryClient(
+            comet_websocket_url.encode('utf-8'),
+            self.err_ptr)
+
+        if self.err_ptr[0] != ffi.NULL:
+            raise Exception(ffi.string(self.err_ptr[0]))
--- tests/test_poktroll_clients.py
+++ tests/test_poktroll_clients.py
@@ -6,12 +6,24 @@ sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), "../src")))
 
 import pytest
 # from src.poktroll_clients import EventsQueryClient
-from poktroll_clients import EventsQueryClient
+from poktroll_clients import *
 
 
 def test_events_query_client():
     client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
-    assert client.selfRef != -1
-    assert client.selfRef != 0
+    # TODO_IN_THIS_COMMIT: refactor constructor to error if selfRef is < 1.
+    assert client.self_ref != -1
+    assert client.self_ref != 0
     assert client.EventsBytes("tm.event='Tx'") != -1
     assert client.EventsBytes("tm.event='Tx'") != 0
+
+
+def test_block_query_client():
+    events_query_client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
+    assert events_query_client.self_ref != -1
+    assert events_query_client.self_ref != 0
+
+    # TODO_IN_THIS_COMMIT: refactor constructor to error if selfRef is < 1.
+    block_query_client = BlockQueryClient("http://127.0.0.1:26657")
+    assert events_query_client.self_ref != -1
+    assert events_query_client.self_ref != 0
