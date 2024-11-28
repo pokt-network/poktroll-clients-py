0f6d3c0f358eb1901f0cbe96d1c4e802219aa06a
Bryan White <bryanchriswhite@gmail.com>
2024-11-28 20:58:02 +0100

    wip: sending tx/msgs from python!

--- src/poktroll_clients.py
+++ src/poktroll_clients.py
@@ -11,13 +11,26 @@ ffi = FFI()
 thisDirPath = path.dirname(path.abspath(__file__))
 
 # TODO_IN_THIS_COMMIT: comment
+# TODO_IN_THIS_COMMIT: refactor into multiple cdefs and move to appropriate files.
 ffi.cdef("""
     typedef int64_t GoRef;
+    
+    void FreeGoMem(GoRef go_ref);
+    
+    GoRef Supply(GoRef go_ref, char **err);
+    GoRef SupplyMany(GoRef *go_refs, int num_go_refs, char **err);
     
     GoRef NewEventsQueryClient(const char* comet_websocket_url);
     GoRef EventsQueryClientEventsBytes(GoRef selfRef, const char* query);
     
     GoRef NewBlockQueryClient(char *comet_websocket_url, char **err);
+    
+    GoRef NewTxContext(char *tcp_url, char **err);
+    
+    GoRef NewBlockClient(GoRef cfg_ref, char **err);
+    
+    GoRef NewTxClient(GoRef cfg_ref, char *signing_key_name, char **err);
+    GoRef SignAndBroadcast(GoRef selfRef, char *msg_any_json, char **err);
 """)
 
 # Load the shared object file
@@ -25,27 +38,176 @@ sharedObjectPath = path.join(thisDirPath, "..", "ext", "libclients.so")
 lib = ffi.dlopen(sharedObjectPath)
 
 
-class EventsQueryClient:
-    self_ref: GoRef
+def check_err(err_ptr: ffi.CData):
+    if err_ptr[0] != ffi.NULL:
+        raise Exception(ffi.string(err_ptr[0]))
+
+
+def check_ref(go_ref: GoRef):
+    assert go_ref != -1
+    assert go_ref != 0
+
+
+class GoManagedMem:
+    """
+    A base class for all objects which embed Go-managed memory.
+
+    Attributes:
+        go_ref: The Go-managed memory reference (int).
+    """
+
+    go_ref: GoRef
+    err_ptr: ffi.CData = ffi.new("char **")
+
+    def __init__(self, go_ref: GoRef):
+        """
+        Constructor for GoManagedMem. Stores the Go-managed memory reference.
+        """
+
+        self.go_ref = go_ref
+
+        check_err(self.err_ptr)
+        check_ref(go_ref)
+
+    def __del__(self):
+        """
+        Destructor for GoManagedMem. Frees the Go-managed memory associated with the reference.
+        """
+
+        lib.FreeGoMem(self.go_ref)
+
+
+class EventsQueryClient(GoManagedMem):
+    """
+    TODO_IN_THIS_COMMIT: comment
+    """
+
+    go_ref: GoRef
+    err_ptr: ffi.CData
 
     def __init__(self, comet_websocket_url: str):
-        self.self_ref = lib.NewEventsQueryClient(comet_websocket_url.encode('utf-8'))
+        go_ref = lib.NewEventsQueryClient(comet_websocket_url.encode('utf-8'))
+        super().__init__(go_ref)
 
     def EventsBytes(self, query: str) -> GoRef:
-        return lib.EventsQueryClientEventsBytes(self.self_ref, query.encode('utf-8'))
+        return lib.EventsQueryClientEventsBytes(self.go_ref, query.encode('utf-8'))
 
-    def free(self):
-        lib.FreeClient(self.self_ref)
 
-class BlockQueryClient:
+class BlockQueryClient(GoManagedMem):
+    """
+    TODO_IN_THIS_COMMIT: comment
+    """
+
     self_ref: GoRef
     err_ptr: ffi.CData
 
     def __init__(self, comet_websocket_url: str):
-        self.err_ptr = ffi.new("char **")
-        self.self_ref = lib.NewBlockQueryClient(
-            comet_websocket_url.encode('utf-8'),
-            self.err_ptr)
+        go_ref = lib.NewBlockQueryClient(comet_websocket_url.encode('utf-8'),
+                                         self.err_ptr)
 
-        if self.err_ptr[0] != ffi.NULL:
-            raise Exception(ffi.string(self.err_ptr[0]))
+        super().__init__(go_ref)
+
+
+def Supply(go_ref: GoRef) -> GoRef:
+    """
+    Supplies a Go-managed memory reference to a returned (Go-managed) depinject config.
+    :param go_ref: The Go-managed memory reference to supply via depinject.
+    :return: The Go-managed memory reference of the resulting depinject config.
+    """
+
+    err_ptr = ffi.new("char **")
+    cfg_ref = lib.Supply(go_ref, err_ptr)
+
+    check_err(err_ptr)
+    check_ref(cfg_ref)
+
+    return cfg_ref
+
+
+def SupplyMany(*go_objs: GoManagedMem) -> GoRef:
+    """
+    Supplies multiple Go-managed memory references to a returned (Go-managed) depinject config.
+    :param go_objs: The Go-managed memory objects to supply via depinject.
+    :return: The Go-managed memory reference of the resulting depinject config.
+    """
+
+    go_refs = [go_obj.go_ref for go_obj in go_objs]
+    cgo_refs = ffi.new("GoRef[]", go_refs)
+    err_ptr = ffi.new("char **")
+
+    cfg_ref = lib.SupplyMany(cgo_refs, len(go_objs), err_ptr)
+
+    check_err(err_ptr)
+    check_ref(cfg_ref)
+
+    return cfg_ref
+
+
+class TxContext(GoManagedMem):
+    """
+    TODO_IN_THIS_COMMIT: comment
+    """
+
+    go_ref: GoRef
+    err_ptr: ffi.CData
+
+    def __init__(self, tcp_url: str):
+        """
+        Constructor for TxContext.
+        :param tcp_url: The gRPC URL for the client to use (e.g. tcp://127.0.0.1:26657).
+        """
+
+        go_ref = lib.NewTxContext(tcp_url.encode('utf-8'), self.err_ptr)
+        super().__init__(go_ref)
+
+
+class BlockClient(GoManagedMem):
+    """
+    TODO_IN_THIS_COMMIT: comment
+    """
+
+    go_ref: GoRef
+    err_ptr: ffi.CData
+
+    def __init__(self, cfg_ref: GoRef):
+        """
+        Constructor for BlockClient.
+        :param cfg_ref: A Go-managed memory reference to a depinject config.
+        """
+
+        go_ref = lib.NewBlockClient(cfg_ref, self.err_ptr)
+        super().__init__(go_ref)
+
+
+class TxClient(GoManagedMem):
+    """
+    TODO_IN_THIS_COMMIT: comment
+    """
+
+    go_ref: GoRef
+    err_ptr: ffi.CData
+
+    def __init__(self, cfg_ref: GoRef, signing_key_name: str):
+        """
+        Constructor for TxClient.
+        :param cfg_ref: A Go-managed memory reference to a depinject config.
+        """
+
+        go_ref = lib.NewTxClient(cfg_ref, signing_key_name.encode('utf-8'), self.err_ptr)
+        super().__init__(go_ref)
+
+        check_err(self.err_ptr)
+        check_ref(go_ref)
+
+    def SignAndBroadcast(self, msg_any_json: str) -> GoRef:
+        """
+        Signs and broadcasts a transaction.
+        :param msg_any_json: The transaction to sign and broadcast.
+        :return: The Go-managed memory reference of the err channel which is
+        notified if the resulting transaction fails the CheckTX ABCI++ method.
+        """
+
+        err_ch_ref =  lib.SignAndBroadcast(self.go_ref, msg_any_json.encode('utf-8'), self.err_ptr)
+
+        check_err(self.err_ptr)
+        check_ref(err_ch_ref)
--- tests/test_poktroll_clients.py
+++ tests/test_poktroll_clients.py
@@ -5,25 +5,76 @@ from os import path
 sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), "../src")))
 
 import pytest
-# from src.poktroll_clients import EventsQueryClient
 from poktroll_clients import *
 
 
 def test_events_query_client():
     client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
     # TODO_IN_THIS_COMMIT: refactor constructor to error if selfRef is < 1.
-    assert client.self_ref != -1
-    assert client.self_ref != 0
     assert client.EventsBytes("tm.event='Tx'") != -1
     assert client.EventsBytes("tm.event='Tx'") != 0
 
 
+def test_depinject_supply():
+    client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
+    cfg_ref = Supply(client.go_ref)
+
+
 def test_block_query_client():
     events_query_client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
-    assert events_query_client.self_ref != -1
-    assert events_query_client.self_ref != 0
-
-    # TODO_IN_THIS_COMMIT: refactor constructor to error if selfRef is < 1.
     block_query_client = BlockQueryClient("http://127.0.0.1:26657")
-    assert events_query_client.self_ref != -1
-    assert events_query_client.self_ref != 0
+
+
+def test_depinject_supply_many():
+    events_query_client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
+    block_query_client = BlockQueryClient("http://127.0.0.1:26657")
+
+
+def test_tx_context():
+    tx_ctx = TxContext("tcp://127.0.0.1:26657")
+
+
+def test_block_client():
+    events_query_client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
+    block_query_client = BlockQueryClient("http://127.0.0.1:26657")
+    cfg_ref = SupplyMany(events_query_client, block_query_client)
+    block_client = BlockClient(cfg_ref)
+
+
+def test_tx_client():
+    cfg_ref = get_tx_client_deps()
+    tx_client = TxClient(cfg_ref, "faucet")
+
+
+def test_sign_and_broadcast():
+    cfg_ref = get_tx_client_deps()
+    tx_client = TxClient(cfg_ref, "faucet")
+
+    # NB: MsgSend from faucet to app1.
+    send_msg_any_json = """
+    {
+      "@type": "type.googleapis.com/cosmos.bank.v1beta1.MsgSend",
+      "from_address": "pokt1awtlw5sjmw2f5lgj8ekdkaqezphgz88rdk93sk",
+      "to_address": "pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4",
+      "amount": [
+        {
+          "denom": "upokt",
+          "amount": "100000000"
+        }
+      ]
+    }
+    """
+
+    err_ch_ref = tx_client.SignAndBroadcast(send_msg_any_json)
+
+
+def get_tx_client_deps() -> GoRef:
+    events_query_client = EventsQueryClient("ws://127.0.0.1:26657/websocket")
+    block_query_client = BlockQueryClient("http://127.0.0.1:26657")
+
+    cfg_ref = SupplyMany(events_query_client, block_query_client)
+    block_client = BlockClient(cfg_ref)
+
+    tx_ctx = TxContext("tcp://127.0.0.1:26657")
+
+    return SupplyMany(events_query_client, block_client, tx_ctx)
