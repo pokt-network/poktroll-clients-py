e6542b2abf4f1f46da4cfdd4a71a4b4d47dfc3db
Bryan White <bryanchriswhite@gmail.com>
2024-12-02 12:01:43 +0100

    test: stake app3 instead of app1

--- tests/test_poktroll_clients.py
+++ tests/test_poktroll_clients.py
@@ -5,7 +5,7 @@ from src.poktroll_clients import *
 from gen.cosmos.base.v1beta1.coin_pb2 import Coin
 from gen.poktroll.shared.service_pb2 import ApplicationServiceConfig
 from gen.poktroll.shared.service_pb2 import Service
-from gen.poktroll.application.tx_pb2 import MsgStakeApplication
+from gen.poktroll.application.tx_pb2 import MsgStakeApplication, MsgUnstakeApplication
 
 
 def test_events_query_client():
@@ -69,19 +69,23 @@ def test_sign_and_broadcast_any():
 
 
 def test_sign_and_broadcast():
-    # NB: Staking app1 for svc1.
-    msg = MsgStakeApplication(address="pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4",
-                              stake=(Coin(denom="upokt", amount="100000000")),
-                              services=[(ApplicationServiceConfig(service_id="svc1"))])
+    # NB: Unstaking app1.
+    # unstakeApp1Msg = MsgUnstakeApplication(address="pokt1mrqt5f7qh8uxs27cjm9t7v9e74a9vvdnq5jva4")
+
+    stakeApp3Msg = MsgStakeApplication(address="pokt1lqyu4v88vp8tzc86eaqr4lq8rwhssyn6rfwzex",
+                                       stake=(Coin(denom="upokt", amount="100000000")),
+                                       services=[(ApplicationServiceConfig(service_id="svc1"))])
 
     cfg_ref = get_tx_client_deps()
-    tx_client = TxClient(cfg_ref, "app1")
+    tx_client = TxClient(cfg_ref, "app3")
 
-    fut = tx_client.SignAndBroadcast(msg)
+    # fut = tx_client.SignAndBroadcast(unstakeApp1Msg)
+    # fut.result()
+    # # loop = asyncio.get_running_loop()
+    # # result = loop.run_until_complete(fut)
+
+    fut = tx_client.SignAndBroadcast(stakeApp3Msg)
     fut.result()
-
-    # loop = asyncio.get_running_loop()
-    # result = loop.run_until_complete(fut)
 
 
 def get_tx_client_deps() -> go_ref:
