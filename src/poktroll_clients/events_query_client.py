94bc8f138a9f1b3900b21e9be87a6f7bc99bd08d
Bryan White <bryanchriswhite@gmail.com>
2024-12-20 00:35:58 +0100

    refactor: rename self_ref

--- src/poktroll_clients/events_query_client.py
+++ src/poktroll_clients/events_query_client.py
@@ -11,8 +11,8 @@ class EventsQueryClient(GoManagedMem):
     err_ptr: ffi.CData
 
     def __init__(self, query_node_rpc_websocket_url: str):
-        go_ref = libpoktroll_clients.NewEventsQueryClient(query_node_rpc_websocket_url.encode('utf-8'))
-        super().__init__(go_ref)
+        self_ref = libpoktroll_clients.NewEventsQueryClient(query_node_rpc_websocket_url.encode('utf-8'))
+        super().__init__(self_ref)
 
     def EventsBytes(self, query: str) -> go_ref:
         return libpoktroll_clients.EventsQueryClientEventsBytes(self.go_ref, query.encode('utf-8'))
