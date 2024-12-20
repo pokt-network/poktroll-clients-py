479764b6bf58701bcfea8dae03451f5c4c0f19f2
Bryan White <bryanchriswhite@gmail.com>
2024-12-20 01:05:19 +0100

    refactor: imports (#3)
    refactor: imports

--- src/poktroll_clients/__init__.py
+++ src/poktroll_clients/__init__.py
@@ -1,12 +1,12 @@
 # Add generated protobuf types to the module path.
 from os import path
 
+from .go_memory import ffi, libpoktroll_clients, go_ref, check_err, check_ref, GoManagedMem
+from .depinject import Supply, SupplyMany
+from .events_query_client import EventsQueryClient
 from .block_client import BlockClient, BlockQueryClient
 from .tx_context import TxContext
 from .tx_client import TxClient
-from .events_query_client import EventsQueryClient
-from .depinject import Supply, SupplyMany
-from .go_memory import go_ref
 
 __all__ = [
     'BlockClient',
@@ -16,5 +16,10 @@ __all__ = [
     'EventsQueryClient',
     'Supply',
     'SupplyMany',
+    'ffi',
     'go_ref',
+    'check_err',
+    'check_ref',
+    'GoManagedMem',
+    'libpoktroll_clients',
 ]
